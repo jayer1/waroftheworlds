ead39f6c70e43e2d47de25087746cce1e03d4542
jayer1 <jayer1@my.wctc.edu>
2019-02-19 10:45:30 -0600

    Trying to figure out null count issue

---
+++ src/us/mattgreen/Main.java
@@ -0,0 +1,69 @@
+package us.mattgreen;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+
+    //private final static FileInput indata = new FileInput("the_book.csv");
+    private final static FileInput indata = new FileInput("newtext.txt");
+    private final static Map<String, Integer> map = new HashMap<String, Integer>();
+    Map<String, List<Main>> counts = new HashMap<String, List<Main>>();
+    Map<String, Integer> frequency = new HashMap<>();
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        String line;
+        String[] words;
+        ArrayList<Integer> wordCounts = new ArrayList<>();
+        int count = 0;
+        String currentWord = "the";
+        while ((line = indata.fileReadLine()) != null) {
+            // Remove anything that's not a letter or space
+            line = line.replaceAll("[^a-zA-Z ]", "")
+                    .toLowerCase().trim();
+
+            // Don't process lines with no characters
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            // Split string over one or more spaces
+            words = line.split(" +");
+
+            // Look for each word in the map
+            for (String word : words) {
+                // This word isn't yet a key, init count to 1
+                if (word == null || word.trim().equals("")) {
+                    continue;
+                }
+                String processed = word.toLowerCase();
+                if (frequency.containsKey(processed)) {
+                    frequency.put(processed, frequency.get(processed + 1));
+                } else {
+                    frequency.put(processed, 1);
+                }
+            }
+            // Loop over entries in the map, getting each key/value pair
+
+        }
+
+        System.out.println(frequency);
+        int mostFrequentlyUsed = 0;
+        String theWord = null;
+        for (String word : frequency.keySet()) {
+            Integer theVal = frequency.get(word);
+            if (theVal > mostFrequentlyUsed) {
+                mostFrequentlyUsed = theVal;
+                theWord = word;
+            }
+        }
+        //System.out.println("The most frequently used word is " + theWord + " amount " + mostFrequentlyUsed);
+    }
+}
